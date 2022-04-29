e22b12c50e91b51a0b81fc2eb527bc40e3fec7af
Zelda_Fan <78901316+ZeldaFan0225@users.noreply.github.com>
2022-04-29 19:47:59 +0200

    added new keys

---
+++ resources/main.js
@@ -0,0 +1,27 @@
+const fs = require("fs")
+
+const en = JSON.parse(fs.readFileSync('./en.json'));
+let template = {};
+
+for (let k in en) template[k] = "";
+template = JSON.stringify(template);
+let missing_values = []
+
+for (const file of fs.readdirSync('.')) {
+    if ((file.length != 7 || file.endsWith("js")) && file !== "TEMPLATE.json") continue
+
+    const json = JSON.parse(fs.readFileSync('./' + file))
+    const o = JSON.parse(template)
+
+    for (let k in json) {
+        o[k] = json[k];
+        let keys_en = /{(\d+)}/g.exec(en[k])
+        if(keys_en && json[k]) {
+           if(keys_en.length !== /{(\d+)}/g.exec(json[k])?.length) missing_values.push(k) 
+        }
+    }
+
+    fs.writeFileSync('./' + file, JSON.stringify(o, null, 4))
+}
+
+console.log(missing_values)
