e218c9f65d6db4587e3d66db15e4c9da73bcdc63
ZeldaFan0225 <78901316+ZeldaFan0225@users.noreply.github.com>
2022-09-05 14:13:16 +0200

    small refactor of package

---
+++ index.d.ts
@@ -0,0 +1,40 @@
+export declare enum Locale {
+    EnglishUS = "en-US",
+    EnglishGB = "en-GB",
+    Bulgarian = "bg",
+    ChineseCN = "zh-CN",
+    ChineseTW = "zh-TW",
+    Croatian = "hr",
+    Czech = "cs",
+    Danish = "da",
+    Dutch = "nl",
+    Finnish = "fi",
+    French = "fr",
+    German = "de",
+    Greek = "el",
+    Hindi = "hi",
+    Hungarian = "hu",
+    Italian = "it",
+    Japanese = "ja",
+    Korean = "ko",
+    Lithuanian = "lt",
+    Norwegian = "no",
+    Polish = "pl",
+    PortugueseBR = "pt-BR",
+    Romanian = "ro",
+    Russian = "ru",
+    SpanishES = "es-ES",
+    Swedish = "sv-SE",
+    Thai = "th",
+    Turkish = "tr",
+    Ukrainian = "uk",
+    Vietnamese = "vi"
+}
+
+export declare type LocaleString = `${Locale}`;
+
+export declare class SlashBotLocalizationManager {
+    constructor()
+    loadLanguages(): undefined;
+    getTranslation(locale: LocaleString, key: string, ...args: (string | number)[]): string;
+}
