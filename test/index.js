182106483b01258301f71ce97bf19c548484caeb
ZeldaFan0225 <78901316+ZeldaFan0225@users.noreply.github.com>
2022-11-03 18:05:29 +0100

    add ability to reload languages without restarting

--- test/index.js
+++ test/index.js
@@ -2,3 +2,5 @@ const { SlashBotLocalizationManager } = require( "../index" );
 
 const languages = new SlashBotLocalizationManager()
 console.log(languages.getTranslation("de", "NONE"))
+languages.loadLanguages()
+console.log(languages.getTranslation("en", "NONE"))
